12274d188e80d4ba776a08a0fc7b6e34c1d2efee
ujooju <141132942+ujooju@users.noreply.github.com>
2025-01-04 14:49:55 +0000

    added sign_in and sign_in pages
    	modified:   handlers/handlers.go
    	modified:   handlers/mux.go
    	modified:   pages/index.html
    	new file:   pages/sign_in.html
    	new file:   pages/sign_up.html
    	new file:   static/styles/styles.css

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -1,7 +1,6 @@
 package handlers
 
 import (
-	"fmt"
 	"io"
 	"log"
 	"net/http"
@@ -18,23 +17,37 @@ func home_page(w http.ResponseWriter, r *http.Request) {
 	w.Write(data)
 }
 
-func start_page(w http.ResponseWriter, r *http.Request) {
+func auth(w http.ResponseWriter, r *http.Request) {
 	session, err := r.Cookie("session_id")
 	if err == http.ErrNoCookie {
-		http.Redirect(w, r, "/auth", http.StatusSeeOther)
+		http.Redirect(w, r, "/sign_in", http.StatusSeeOther)
 		return
 	}
 	w.Write([]byte(session.String()))
 }
 
-func auth_page(w http.ResponseWriter, r *http.Request) {
-	expires := time.Now().Add(time.Hour)
-	fmt.Println(expires)
+func sign_in(w http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodGet {
+		f, _ := os.Open("pages/sign_in.html")
+		data, _ := io.ReadAll(f)
+		w.Write(data)
+		return
+	}
+	expires := time.Now().Add(time.Second * 15)
 	newSession := http.Cookie{
 		Name:    "session_id",
 		Value:   "123",
 		Expires: expires,
 	}
 	http.SetCookie(w, &newSession)
-	http.Redirect(w, r, "/start_page", http.StatusFound)
+	http.Redirect(w, r, "/auth", http.StatusFound)
+}
+
+func sign_up(w http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodGet {
+		f, _ := os.Open("pages/sign_up.html")
+		data, _ := io.ReadAll(f)
+		w.Write(data)
+		return
+	}
 }
--- handlers/mux.go
+++ handlers/mux.go
@@ -2,10 +2,17 @@ package handlers
 
 import "net/http"
 
+func FileServer() http.Handler {
+	fileServer := http.StripPrefix("/static", http.FileServer(http.Dir("./static/")))
+	return fileServer
+}
+
 func Init_mux() http.Handler {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", home_page)
-	mux.HandleFunc("/start_page", start_page)
-	mux.HandleFunc("/auth", auth_page)
+	mux.HandleFunc("/auth", auth)
+	mux.HandleFunc("/sign_in", sign_in)
+	mux.HandleFunc("/sign_up", sign_up)
+	mux.Handle("/static/", FileServer())
 	return mux
 }
